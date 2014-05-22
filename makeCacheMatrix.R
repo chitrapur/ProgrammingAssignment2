9d1731ec1ee7e996a1d72ee4c390a58f2fe15a63
chitrapur <kalawar@yahoo.com>
2014-05-22 19:49:56 -0400

    commit makeCacheMatrix.R ProgrammingAssignment2

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,6 +1,9 @@
 #The following pair of functions cache the inverse of a matrix.
 
-#The first function, makeVector creates a special "vector", which is really a list containing #a function to a) set the value of the matrix; b) get the value of the matrix; c) set the value #of the inverse (with the function solve); d) get the value of the inverse.
+#The first function, makeVector creates a special "vector", 
+#which is really a list containing #a function to a) set the value of 
+#the matrix; b) get the value of the matrix; c) set the value 
+#of the inverse (with the function solve); d) get the value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -16,7 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-#The following function calculates the mean of the special "matrix" created with the above #function. However, it first checks to see if the inverse has already been calculated. If so, #it gets the inverse from the cache and skips the computation. Otherwise, it calculates the #inverse of the data and sets the value of the inverse in the cache via the setsolve #function.
+#The following function calculates the mean of the special "matrix" 
+#created with the above #function. However, it first checks to see if 
+#the inverse has already been calculated. If so, #it gets the inverse 
+#from the cache and skips the computation. Otherwise, it calculates the 
+#inverse of the data and sets the value of the inverse in the cache via 
+#the setsolve function.
 
 cachesolve <- function(x, ...) {
         inv <- x$getsolve()
@@ -31,5 +39,3 @@ cachesolve <- function(x, ...) {
 }
 
 
-A<-function(x=commutation.matrix()){
-y<-solve(x)}
